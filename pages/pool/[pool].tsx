477981775646eeb46cf0db20b38be3fc4ac86337
Norman Saade <norman.saade@gmail.com>
2022-12-05 14:38:05 -1000

    fix hydration error

--- pages/pool/[pool].tsx
+++ pages/pool/[pool].tsx
@@ -2,8 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useAccount, useContractEvent } from "wagmi"
-import { useState } from 'react';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useState, useEffect } from 'react';
 import Navbar from 'components/shared/navbar';
 import Footer from 'components/shared/footer';
 import { StakeFormComponent } from 'components/staker/stakeFormComponent';
@@ -18,13 +17,21 @@ const Pool: NextPage = () => {
   const router = useRouter()
   const poolAddress = router.query.pool as string ? router.query.pool as string : "notSet"
 
-  let etherscanLink = ""
-
+  const [isDefinitelyConnected, setIsDefinitelyConnected] = useState(false);
   const { isConnected, address } = useAccount()
   const [stakeAmount, setStakeAmount] = useState<string>("0");
   const [isDepositing, setIsDepositing] = useState(false);
   const { data: poolOwner } = usePoolOwner({ address: poolAddress as string });
   const { data, write: deposit } = useDeposit({ address: poolAddress as string, val: stakeAmount });
+  const etherscanLink = `https://goerli.etherscan.io/tx/${data?.hash}`;
+
+  useEffect(() => {
+    if (isConnected) {
+        setIsDefinitelyConnected(true);
+    } else {
+        setIsDefinitelyConnected(false);
+    }
+}, [address]);
 
   // useContractEvent({
   //   addressOrName: poolAddress.toString(),
@@ -35,14 +42,10 @@ const Pool: NextPage = () => {
   //   },
   // })
 
-  if(data){
-    etherscanLink = `https://goerli.etherscan.io/tx/${data.hash}`
-  }
-
   return (
     <div className="bg-gradient-to-r from-cyan-400 to-blue-300 min-h-screen" data-theme="winter">
       <Head>
-        <title>FRENS | stake </title>
+        <title>FRENS Pool </title>
         <meta
           name="description"
           content="stake eth via ur trusted degen"
@@ -58,61 +61,13 @@ const Pool: NextPage = () => {
         <div className='w-3/5 border-2 border-violet-500 rounded-md bg-white mt-6'>
           {/* <DepositProgressBarComponent /> */}
 
-          <StakeFormComponent setStake={setStakeAmount}/>
-          
-          <div className='flex justify-center mt-2 mb-4'>
-            { isConnected ?
-              <div>
-                { isDepositing ? 
-                  <button 
-                    className="btn btn-primary text-white" 
-                    disabled
-                    >
-                    Processing
-                  </button>
-                  :
-                  <button 
-                    className="btn text-white bg-gradient-to-r from-pink-500 to-violet-500" 
-                    onClick={() => {
-                      deposit();
-                      setIsDepositing(true);
-                    }}
-                    >
-                    Stake
-                  </button>
-                }
-              </div>
-              :
-              <ConnectButton/>
-            }
-          </div>
-          { data ? 
-            <div className="px-6 mb-4">
-              <div className="my-2 text-center">
-                  Your deposit is being processed. <br/> Pls kindly refresh the page once the deposit is done.
-              </div>
-              <div className="flex justify-center">
-                  <div role="status">
-                      <svg aria-hidden="true" className="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-cyan-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                      </svg>
-                      <span className="sr-only">Loading...</span>
-                  </div>
-                  <a className="underline text-blue-500 pt-1" href={etherscanLink} target="_blank" rel="noopener noreferrer">
-                      view tx on etherscan
-                  </a>
-              </div>
-            </div>
-            :
-            <div></div>
-          }
+          <StakeFormComponent poolAddress={poolAddress} />
 
           <div className='border border-violet-500 rounded-md mx-4'></div>
           <PoolInfo address={poolAddress} />
         </div>
 
-        <div className={`w-3/5 p-4 my-6 border-2 border-violet-500 rounded-md bg-white ${isConnected ? "block" : "hidden"}`}>
+        <div className={`w-3/5 p-4 my-6 border-2 border-violet-500 rounded-md bg-white ${isDefinitelyConnected ? "block" : "hidden"}`}>
           <div className='text-center font-bold my-2'>Current pool stakes *(yours for now)*</div>
           <NftGallery />
         </div>
