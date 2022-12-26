39505bdc6d9447a1b87dea02f7ebde7ff274412c
normadn <norman.saade@gmail.com>
2022-12-26 13:46:30 -1000

    build bug fix ....

--- pages/run/[pool].tsx
+++ pages/run/[pool].tsx
@@ -26,64 +26,67 @@ const Operator: NextPage = () => {
   //   }
   // }, [poolAddress])
 
-  return (
-    <div className="bg-gradient-to-r from-cyan-400 to-blue-300" data-theme="winter">
-      <Head>
-        <title>FRENS Pool </title>
-        <meta
-          name="description"
-          content="stake eth via ur trusted degen"
-        />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧑‍🤝‍🧑</text></svg>" />
-      </Head>
+  if(poolAddress) {
+    return (
+      <div className="bg-gradient-to-r from-cyan-400 to-blue-300" data-theme="winter">
+        <Head>
+          <title>FRENS Pool </title>
+          <meta
+            name="description"
+            content="stake eth via ur trusted degen"
+          />
+          <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧑‍🤝‍🧑</text></svg>" />
+        </Head>
 
-      <Navbar />
+        <Navbar />
 
-      <main className="flex flex-col justify-center items-center min-h-[93vh]">
-        <div className="w-11/12 md:w-2/3 text-center flex flex-col items-center border-2 border-violet-500 rounded-md mb-4 p-3 bg-white">
-          <h1 className="text-3xl font-bold">
-            Pool Information
-          </h1>
-          <div className="block w-2/3 mt-2">
-            <a 
-              className='underline'
-              href={`https://app.frens.fun/pool/${poolAddress}`}
-            >
-              {`https://app.frens.fun/pool/${poolAddress}`}
-            </a>
-            <PoolInfo address={poolAddress} />
+        <main className="flex flex-col justify-center items-center min-h-[93vh]">
+          <div className="w-11/12 md:w-2/3 text-center flex flex-col items-center border-2 border-violet-500 rounded-md mb-4 p-3 bg-white">
+            <h1 className="text-3xl font-bold">
+              Pool Information
+            </h1>
+            <div className="block w-2/3 mt-2">
+              <a 
+                className='underline'
+                href={`https://app.frens.fun/pool/${poolAddress}`}
+              >
+                {`https://app.frens.fun/pool/${poolAddress}`}
+              </a>
+              <PoolInfo poolAddress={poolAddress} />
+            </div>
           </div>
-        </div>
 
-        <div className="w-11/12 md:w-2/3 text-center flex flex-col items-center border-2 border-violet-500 rounded-md mb-4 p-3 bg-white">
-          <h1 className="text-3xl font-bold">
-            3️⃣ Run SSV-validator
-          </h1>
-          <div className="block">
-            <div>
-              <div className="my-2 p-2 border border-slate-700 rounded-md">
-                <p>1. create staking keys using this command:</p>
-                <div><code>deposit new-mnemonic --eth1_withdrawal_address {poolAddress}</code></div>
-              </div>
-              <div className="my-2 p-2 border border-slate-700 rounded-md">
-                <div>2. upload the deposit file here</div>
-                <DropKeys onFileReceived={(data) => {
-                  const depositData = JSON.parse(data);
-                  setDepositFileData(depositData[0]);
-                }} />
-                <Deposit address={poolAddress as string} depositdata={depositFileData} />
-              </div>
-              <div className="my-2 p-2 border border-slate-700 rounded-md">
-                <SelectOperator setTokenCode={setTokenCode}/>
-                <GiveAllowance onFileReceived={undefined}/>
+          <div className="w-11/12 md:w-2/3 text-center flex flex-col items-center border-2 border-violet-500 rounded-md mb-4 p-3 bg-white">
+            <h1 className="text-3xl font-bold">
+              3️⃣ Run SSV-validator
+            </h1>
+            <div className="block">
+              <div>
+                <div className="my-2 p-2 border border-slate-700 rounded-md">
+                  <p>1. create staking keys using this command:</p>
+                  <div><code>deposit new-mnemonic --eth1_withdrawal_address {poolAddress}</code></div>
+                </div>
+                <div className="my-2 p-2 border border-slate-700 rounded-md">
+                  <div>2. upload the deposit file here</div>
+                  <DropKeys onFileReceived={(data) => {
+                    const depositData = JSON.parse(data);
+                    setDepositFileData(depositData[0]);
+                  }} />
+                  <Deposit address={poolAddress as string} depositdata={depositFileData} />
+                </div>
+                <div className="my-2 p-2 border border-slate-700 rounded-md">
+                  <SelectOperator setTokenCode={setTokenCode}/>
+                  <GiveAllowance onFileReceived={undefined}/>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </main>
-      <Footer/>
-    </div >
-  );
+        </main>
+        <Footer/>
+      </div >
+    );
+  }
+  return(<div>loading animation!</div>)
 };
 
 export default Operator;
