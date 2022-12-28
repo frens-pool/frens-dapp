efd1afe5792a3e3c13c473f729b8420788ea6c8f
normadn <norman.saade@gmail.com>
2022-12-28 08:19:52 -1000

    ssv package to split key

--- components/operator/ssvRegisterValidator.tsx
+++ components/operator/ssvRegisterValidator.tsx
@@ -1,3 +1,4 @@
+import { ISharesKeyPairs, SSVKeys } from 'ssv-keys';
 import { useAllowance } from '../../hooks/write/useAllowance';
 
 export const SSVRegisterValidator = ({ keystoredata }: { keystoredata: any }) => {
@@ -5,6 +6,11 @@ export const SSVRegisterValidator = ({ keystoredata }: { keystoredata: any }) =>
         spender: "",
         value: ""
     });
+    const ssvKeys = new SSVKeys();
+    // follow new steps at https://docs.ssv.network/developers/tools/ssv-key-distributor/key-distributer-sdk
+
+    // then register with ethers
+    
 
     return (
         <>
