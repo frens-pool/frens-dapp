12d67f8307b10420a191b0c7277913962a88e58f
normadn <norman.saade@gmail.com>
2022-12-28 08:20:21 -1000

    ssv error because of fs ... any suggestions?

--- components/operator/ssvRegisterValidator.tsx
+++ components/operator/ssvRegisterValidator.tsx
@@ -6,11 +6,11 @@ export const SSVRegisterValidator = ({ keystoredata }: { keystoredata: any }) =>
         spender: "",
         value: ""
     });
+    // throws error for now
     const ssvKeys = new SSVKeys();
     // follow new steps at https://docs.ssv.network/developers/tools/ssv-key-distributor/key-distributer-sdk
 
     // then register with ethers
-    
 
     return (
         <>
