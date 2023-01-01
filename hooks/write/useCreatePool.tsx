9fe91f10642208d7c8bb4e2d5bc48345ebbc90e9
normadn <norman.saade@gmail.com>
2023-01-01 11:41:11 -1000

    use of correct addresses

--- hooks/write/useCreatePool.tsx
+++ hooks/write/useCreatePool.tsx
@@ -2,23 +2,22 @@ import {
   usePrepareContractWrite,
   useContractWrite,
   useNetwork,
-  useAccount
+  useAccount,
 } from "wagmi";
 
 import { FrensContracts } from "utils/contracts";
 
 export function useCreatePool() {
   const { chain } = useNetwork();
-  const { address: ownerAddress } = useAccount()
-  
-  
+  const { address: ownerAddress } = useAccount();
+
   const { config } = usePrepareContractWrite({
     address: FrensContracts.StakingPoolFactory.address,
     abi: FrensContracts.StakingPoolFactory.abi,
-    functionName: 'create',
-    args: [ ownerAddress ],
-  })
-  const { data, isLoading, isSuccess, write } = useContractWrite(config)
+    functionName: "create",
+    args: [ownerAddress, false],
+  });
+  const { data, isLoading, isSuccess, write } = useContractWrite(config);
 
   return { data, isLoading, isSuccess, write };
 }
--- pages/pool/[pool].tsx
+++ pages/pool/[pool].tsx
@@ -1,15 +1,15 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { useAccount } from "wagmi"
-import { useState } from 'react';
-import Navbar from 'components/shared/navbar';
-import Footer from 'components/shared/footer';
-import { StakeFormComponent } from 'components/staker/stakeFormComponent';
-import { StakeForm } from 'components/staker/stakeForm';
-import { OperatorWidget } from 'components/staker/operatorWidget';
-import { PoolInfo } from 'components/shared/poolInfo';
-import { NftGallery } from 'components/staker/nftGallery';
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useAccount } from "wagmi";
+import { useState } from "react";
+import Navbar from "components/shared/navbar";
+import Footer from "components/shared/footer";
+import { StakeFormComponent } from "components/staker/stakeFormComponent";
+import { StakeForm } from "components/staker/stakeForm";
+import { OperatorWidget } from "components/staker/operatorWidget";
+import { PoolInfo } from "components/shared/poolInfo";
+import { NftGallery } from "components/staker/nftGallery";
 
 const Pool: NextPage = () => {
   const router = useRouter();
@@ -18,49 +18,61 @@ const Pool: NextPage = () => {
   const [isDepositing, setIsDepositing] = useState(false);
   const { isConnected } = useAccount();
 
-  if(poolAddress) { // && isConnected
+  if (poolAddress) {
     return (
-      <div className="bg-gradient-to-r from-cyan-400 to-blue-300 min-h-screen" data-theme="winter">
+      <div
+        className="bg-gradient-to-r from-cyan-400 to-blue-300 min-h-screen"
+        data-theme="winter"
+      >
         <Head>
           <title>FRENS Pool</title>
-          <meta
-            name="description"
-            content="stake eth via ur trusted degen"
+          <meta name="description" content="stake eth via ur trusted degen" />
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧑‍🤝‍🧑</text></svg>"
           />
-          <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧑‍🤝‍🧑</text></svg>" />
         </Head>
-        
+
         <Navbar />
-  
+
         <main className="flex flex-col justify-center items-center min-h-[93vh]">
-              
           <OperatorWidget poolAddress={poolAddress.toString()} />
-  
-          <div className='w-3/5 border-2 border-violet-500 rounded-md bg-white mt-6'>  
+
+          <div className="w-3/5 border-2 border-violet-500 rounded-md bg-white mt-6">
             {/* <StakeFormComponent poolAddress={poolAddress.toString()} isDepositing={isDepositing} setIsDepositing={setIsDepositing} /> */}
-            <StakeForm poolAddress={poolAddress.toString()} isDepositing={isDepositing} setIsDepositing={setIsDepositing} />
-            <div className='border border-violet-500 rounded-md mx-4'></div>
+            <StakeForm
+              poolAddress={poolAddress.toString()}
+              isDepositing={isDepositing}
+              setIsDepositing={setIsDepositing}
+            />
+            <div className="border border-violet-500 rounded-md mx-4"></div>
             <PoolInfo poolAddress={poolAddress.toString()} />
           </div>
-  
-          <div className={`w-3/5 p-4 my-6 border-2 border-violet-500 rounded-md bg-white ${isConnected ? "block" : "block"}`}>
-            <div className='text-center font-bold my-2'>Pool stakes</div>
-            {isConnected ? 
-              <NftGallery poolAddress={poolAddress.toString()} isDepositing={isDepositing}/>
-              :
+
+          <div
+            className={`w-3/5 p-4 my-6 border-2 border-violet-500 rounded-md bg-white ${
+              isConnected ? "block" : "block"
+            }`}
+          >
+            <div className="text-center font-bold my-2">Pool stakes</div>
+            {isConnected ? (
+              <NftGallery
+                poolAddress={poolAddress.toString()}
+                isDepositing={isDepositing}
+              />
+            ) : (
               <div className="flex flex-col items-center justify-center">
-                  <div className="">Connect wallet to see 🧐</div>
+                <div className="">Connect wallet to see 🧐</div>
               </div>
-            }
+            )}
           </div>
-  
         </main>
-        <Footer/>
-      </div >
+        <Footer />
+      </div>
     );
   }
 
-  return(<div>loading animation!</div>)
+  return <div>loading animation!</div>;
 };
 
 export default Pool;
