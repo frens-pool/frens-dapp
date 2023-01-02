b32573772588d90101add7b788ac70011a7bc9b3
normadn <norman.saade@gmail.com>
2023-01-01 16:04:41 -1000

    ssv keysplit in progress

--- components/operator/ssvRegisterValidator.tsx
+++ components/operator/ssvRegisterValidator.tsx
@@ -1,25 +1,65 @@
-import { ISharesKeyPairs, SSVKeys } from 'ssv-keys';
-import { useAllowance } from '../../hooks/write/useAllowance';
+import { useState, useEffect } from "react";
+import { ISharesKeyPairs, SSVKeys } from "ssv-keys";
+import { useAllowance } from "../../hooks/write/useAllowance";
 
-export const SSVRegisterValidator = ({ keystoredata }: { keystoredata: any }) => {
-    const { data: data2, isLoading: isLoading2, isSuccess: isSuccess2, write: allow } = useAllowance({
-        spender: "",
-        value: ""
-    });
-    // throws error for now
+export const SSVRegisterValidator = ({
+  keystoredata,
+}: {
+  keystoredata: any;
+}) => {
+  const [payloadData, setPayloadData] = useState("abc");
+
+  useEffect(() => {
+    console.log("keystoredata", keystoredata);
+    if (keystoredata) {
+      console.log("rdy to populate");
+      calcPayloadForRegisterSSV();
+    }
+  }, [keystoredata]);
+
+  const {
+    data: data2,
+    isLoading: isLoading2,
+    isSuccess: isSuccess2,
+    write: allow,
+  } = useAllowance({
+    spender: "",
+    value: "",
+  });
+
+  const calcPayloadForRegisterSSV = async () => {
     const ssvKeys = new SSVKeys();
-    // follow new steps at https://docs.ssv.network/developers/tools/ssv-key-distributor/key-distributer-sdk
-
-    // then register with ethers
-
-    return (
-        <>
-            <button className="btn btn-primary my-2 mr-2" disabled={!allow} onClick={() => allow?.()}>
-                Allow spending SSV
-            </button>
-            <a className="btn btn-primary" href="https://app.ssv.network/join/validator/enter-key">
-                Register SSV validator
-            </a>
-        </>
+    const privateKey = await ssvKeys.getPrivateKeyFromKeystoreData(
+      keystoredata,
+      "dummy123"
     );
+    console.log("privateKey", privateKey);
+    setPayloadData(privateKey);
+  };
+
+  const registerValiSSV = () => {};
+
+  return (
+    <>
+      <button
+        className="btn btn-primary my-2 mr-2"
+        disabled={!allow}
+        onClick={() => allow?.()}
+      >
+        Allow spending SSV
+      </button>
+      <button
+        className="btn btn-primary my-2 mr-2"
+        onClick={() => registerValiSSV()}
+      >
+        Register SSV validator
+      </button>
+      {/* <a
+        className="btn btn-primary"
+        href="https://app.ssv.network/join/validator/enter-key"
+      >
+        Register SSV validator
+      </a> */}
+    </>
+  );
 };
--- pages/run/[pool].tsx
+++ pages/run/[pool].tsx
@@ -1,44 +1,45 @@
-import { useState, useEffect } from 'react';
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import router, { useRouter } from 'next/router';
-import Navbar from 'components/shared/navbar';
-import Footer from 'components/shared/footer';
-import { SelectOperator } from 'components/operator/selectOperator';
-import { DropKeys } from 'components/operator/dropKeys';
-import { SSVRegisterValidator } from 'components/operator/ssvRegisterValidator';
-import { PoolInfo } from 'components/shared/poolInfo';
-import { Deposit } from 'components/operator/deposit';
+import { useState, useEffect } from "react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import router, { useRouter } from "next/router";
+import Navbar from "components/shared/navbar";
+import Footer from "components/shared/footer";
+import { SelectOperator } from "components/operator/selectOperator";
+import { DropKeys } from "components/operator/dropKeys";
+import { SSVRegisterValidator } from "components/operator/ssvRegisterValidator";
+import { PoolInfo } from "components/shared/poolInfo";
+import { Deposit } from "components/operator/deposit";
 
 const Operator: NextPage = () => {
-  const router = useRouter()
-  const poolAddress = router.query.pool as string
+  const router = useRouter();
+  const poolAddress = router.query.pool as string;
 
   const [depositFileData, setDepositFileData] = useState();
   const [keystoreFileData, setKeystoreFileData] = useState();
 
-  if(poolAddress) {
+  if (poolAddress) {
     return (
-      <div className="bg-gradient-to-r from-cyan-400 to-blue-300" data-theme="winter">
+      <div
+        className="bg-gradient-to-r from-cyan-400 to-blue-300"
+        data-theme="winter"
+      >
         <Head>
           <title>FRENS Pool </title>
-          <meta
-            name="description"
-            content="stake eth via ur trusted degen"
+          <meta name="description" content="stake eth via ur trusted degen" />
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧑‍🤝‍🧑</text></svg>"
           />
-          <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧑‍🤝‍🧑</text></svg>" />
         </Head>
 
         <Navbar />
 
         <main className="flex flex-col justify-center items-center min-h-[93vh]">
           <div className="w-11/12 md:w-2/3 text-center flex flex-col items-center border-2 border-violet-500 rounded-md mb-4 p-3 bg-white">
-            <h1 className="text-3xl font-bold">
-              Pool Information
-            </h1>
+            <h1 className="text-3xl font-bold">Pool Information</h1>
             <div className="block w-2/3 mt-2">
-              <a 
-                className='underline'
+              <a
+                className="underline"
                 href={`https://app.frens.fun/pool/${poolAddress}`}
               >
                 {`https://app.frens.fun/pool/${poolAddress}`}
@@ -48,47 +49,57 @@ const Operator: NextPage = () => {
           </div>
 
           <div className="w-11/12 md:w-2/3 text-center flex flex-col items-center border-2 border-violet-500 rounded-md mb-4 p-3 bg-white">
-            <h1 className="text-3xl font-bold">
-              3️⃣ Run SSV-validator
-            </h1>
+            <h1 className="text-3xl font-bold">3️⃣ Run SSV-validator</h1>
             <div className="block">
               <div>
                 <div className="my-2 p-2 border border-slate-700 rounded-md">
-                  <div>1. reate staking keys</div>
+                  <div>1. create staking keys</div>
                   <div>using this command:</div>
-                  <div><code>deposit new-mnemonic --eth1_withdrawal_address {poolAddress}</code></div>
+                  <div>
+                    <code>
+                      deposit new-mnemonic --eth1_withdrawal_address{" "}
+                      {poolAddress}
+                    </code>
+                  </div>
                   <div>or using wagyu key gen</div>
                 </div>
                 <div className="my-2 p-2 border border-slate-700 rounded-md">
                   <div>2. Deposit ETH</div>
                   <div>upload the deposit file here</div>
-                  <DropKeys onFileReceived={(data) => {
-                    const depositData = JSON.parse(data);
-                    setDepositFileData(depositData[0]);
-                  }} />
-                  <Deposit address={poolAddress as string} depositdata={depositFileData} />
+                  <DropKeys
+                    onFileReceived={(data) => {
+                      const depositData = JSON.parse(data);
+                      setDepositFileData(depositData[0]);
+                    }}
+                  />
+                  <Deposit
+                    address={poolAddress as string}
+                    depositdata={depositFileData}
+                  />
                 </div>
                 <div className="my-2 p-2 border border-slate-700 rounded-md">
                   <div>3. register SSV validator</div>
                   <div>upload the keystore file here</div>
-                  <DropKeys onFileReceived={(data) => {
-                    const depositData = JSON.parse(data);
-                    setDepositFileData(depositData[0]);
-                  }} />
+                  <DropKeys
+                    onFileReceived={(data) => {
+                      const keystoreData = JSON.parse(data);
+                      setKeystoreFileData(keystoreData);
+                    }}
+                  />
                   <div>select ur operators</div>
                   {/* <SelectOperator setTokenCode={setTokenCode}/> */}
                   <div>register validator</div>
-                  <SSVRegisterValidator keystoredata={depositFileData}/>
+                  <SSVRegisterValidator keystoredata={keystoreFileData} />
                 </div>
               </div>
             </div>
           </div>
         </main>
-        <Footer/>
-      </div >
+        <Footer />
+      </div>
     );
   }
-  return(<div>loading animation!</div>)
+  return <div>loading animation!</div>;
 };
 
 export default Operator;
