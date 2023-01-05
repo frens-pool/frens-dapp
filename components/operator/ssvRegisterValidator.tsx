0c72352d60ca1b4274e16361f2cef65780becb2a
normadn <norman.saade@gmail.com>
2023-01-04 20:20:01 -1000

    logic how registerVali will work

--- components/operator/ssvRegisterValidator.tsx
+++ components/operator/ssvRegisterValidator.tsx
@@ -1,19 +1,38 @@
-import { useState, useEffect } from "react";
-import { ISharesKeyPairs, SSVKeys } from "ssv-keys";
+import { ethers } from "ethers";
+import { useSigner } from "wagmi";
 import { useAllowance } from "../../hooks/write/useAllowance";
+import SSVNetwork from "../../utils/SSVNetwork.json";
 
 export const SSVRegisterValidator = ({ payloadData }: { payloadData: any }) => {
-  const {
-    data: data2,
-    isLoading: isLoading2,
-    isSuccess: isSuccess2,
-    write: allow,
-  } = useAllowance({
+  const { data: signer } = useSigner();
+
+  const { write: allow } = useAllowance({
     spender: "",
     value: "",
   });
 
-  const registerValiSSV = () => {};
+  console.log(payloadData);
+
+  const registerSSVValidator = async () => {
+    const ssvNetworkContract = new ethers.Contract(
+      "0xb9e155e65B5c4D66df28Da8E9a0957f06F11Bc04",
+      SSVNetwork.abi,
+      signer
+    );
+    const action = "registerValidator";
+    const unsignedTx = await ssvNetworkContract.populateTransaction[action](
+      ...payloadData
+    );
+
+    console.log(unsignedTx, "unsignedTx");
+
+    const tx = await signer.sendTransaction(unsignedTx);
+
+    console.log("tx pending...");
+    console.log("\x1b[5m...\x1b[0m");
+    const txReceipt = await tx.wait();
+    console.log(txReceipt.status);
+  };
 
   return (
     <>
@@ -26,7 +45,7 @@ export const SSVRegisterValidator = ({ payloadData }: { payloadData: any }) => {
       </button>
       <button
         className="btn btn-primary my-2 mr-2"
-        onClick={() => registerValiSSV()}
+        onClick={() => registerSSVValidator()}
       >
         Register SSV validator
       </button>
