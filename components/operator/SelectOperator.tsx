48bcc89eb43866cd1ba19d9e5c9ae9ccaecb7fb9
normadn <norman.saade@gmail.com>
2023-04-17 13:17:38 -1000

    ssv validator list pre validation

--- components/operator/SelectOperator.tsx
+++ components/operator/SelectOperator.tsx
@@ -1,24 +1,14 @@
 import { useState, useEffect } from "react";
-import { useEventCreate } from "../../hooks/read/useEventCreate";
-import { useCreatePool } from "../../hooks/write/useCreatePool";
 
-export const SelectOperator = ({
-  nextStep
-}: {
-  nextStep: () => void;
-}) => {
-  const [ssvOperators, setssvOperators] = useState([]);
-  const [operatorList, setOperatorList] = useState(<></>);
+export const SelectOperator = ({ nextStep }: { nextStep: () => void }) => {
+  const [ssvOperators, setssvOperators] = useState<any[]>([]);
   const [frenSsvOperatorIDs, setFrenSsvOperatorIDs] = useState([]);
-
-  const { data, write: createPool } = useCreatePool();
-  // console.log(data)
-  useEventCreate();
+  const [selectedOperators, setSelectedOperators] = useState<any[]>([]);
 
   useEffect(() => {
     const fetchOperators = async () => {
       const data = await fetch(
-        "https://api.ssv.network/api/v1/operators/graph?page=1&perPage=10"
+        "https://api.ssv.network/api/v3/prater/operators?page=1&perPage=10&ordering=performance.30d%3Adesc"
       );
       const json = await data.json();
       setssvOperators(json.operators);
@@ -37,42 +27,33 @@ export const SelectOperator = ({
     fetchFrenOperator().catch(console.error);
   }, []);
 
-  // function onCreatePool(): void {
-  //     const inviteToken = Math.random().toString(36).substring(2, INVITATION_TOKEN_LENGTH);
-  //     setTokenCode(inviteToken);
-
-  //     setStep(2);
-
-  //     createPool();
-  // }
-
-  let operatorListLines = ssvOperators?.map((item, i) => {
+  let operatorListRows = ssvOperators?.map((item, i) => {
     return (
       <tr key={i}>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </th>
         <td>
-          <input type="checkbox" />
-          &nbsp;&nbsp;
+          <div className="flex items-center space-x-3">
+            <div>
+              <div className="font-bold">{item.name}</div>
+              <div className="text-sm opacity-50">verified</div>
+            </div>
+          </div>
         </td>
-        {/* <td>{item.name} &nbsp;&nbsp;&nbsp;</td>
-        <td>{parseFloat(item.performance["24h"]).toFixed(2)}%</td> */}
+        <td>
+          {parseFloat(item.performance["30d"]).toFixed(4)}%
+          <br />
+          <span className="badge badge-ghost badge-sm">last 30d</span>
+        </td>
+        <th>
+          <button className="btn btn-ghost btn-xs">details</button>
+        </th>
       </tr>
     );
   });
-
-  let operatorListTable = (
-    <table>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>24h performance</th>
-      </tr>
-      {operatorListLines}
-    </table>
-  );
-
-  // setOperatorList(operatorListTable);
-
-  // console.log(frenSsvOperatorIDs)
 
   return (
     <div className="my-2 p-2">
@@ -90,103 +71,9 @@ export const SelectOperator = ({
               <th></th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" checked />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div>
-                    <div className="font-bold">FREN 1</div>
-                    <div className="text-sm opacity-50">United States</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                99%
-                <br />
-                <span className="badge badge-ghost badge-sm">verified</span>
-              </td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" checked />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div>
-                    <div className="font-bold">Blox 1</div>
-                    <div className="text-sm opacity-50">China</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                95%
-                <br />
-                <span className="badge badge-ghost badge-sm">verified</span>
-              </td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" checked />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div>
-                    <div className="font-bold">FREN 2</div>
-                    <div className="text-sm opacity-50">Russia</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                97%
-                <br />
-                <span className="badge badge-ghost badge-sm">verified</span>
-              </td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-            <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" checked />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div>
-                    <div className="font-bold">Blox 2</div>
-                    <div className="text-sm opacity-50">Brazil</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                98%
-                <br />
-                <span className="badge badge-ghost badge-sm">verified</span>
-              </td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
-          </tbody>
+          <tbody>{operatorListRows}</tbody>
         </table>
       </div>
-      <div className="font-bold">- temporary static -</div>
       <button
         className="mt-2 btn bg-gradient-to-r from-frens-blue to-frens-teal text-white"
         onClick={() => {
@@ -196,17 +83,5 @@ export const SelectOperator = ({
         Next
       </button>
     </div>
-    // <div>
-    //   {/* <div>Create a SSV operated Validator</div> */}
-    //   <div>
-    //     3. Select three other operators to run you DVT secured validator
-    //   </div>
-    //   <div className="flex justify-center">
-    //     <div>{operatorListTable ? operatorListTable : ""}</div>
-    //   </div>
-    //   {/* <button className='btn btn-primary' onClick={() => onCreatePool()}>
-    //             Deposit ETH to Beacon chain
-    //         </button> */}
-    // </div>
   );
 };
