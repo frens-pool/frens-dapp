6d4eeb14b98c6fafe926a60ad7930c1504d1401b
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2023-05-11 14:52:39 +0200

    Show errors from prepare deposit in UI

--- hooks/write/useDeposit.tsx
+++ hooks/write/useDeposit.tsx
@@ -7,15 +7,15 @@ import { FrensContracts } from "utils/contracts";
 
 export function useDeposit({ address, val, proof }: { address: string, val: string, proof: string[] }) {
 
-  const { config } = usePrepareContractWrite({
+  const { config, error: prepare_error } = usePrepareContractWrite({
     address: address,
     abi: FrensContracts.StakingPool.abi,
     functionName: 'depositToPool',
     overrides: {
       value: ethers.utils.parseEther(val),
     },
-    args: [proof],
+    args: [proof]
   })
   const { data, isLoading, isSuccess, isError, write, error, status } = useContractWrite(config)
-  return { data, isLoading, isSuccess, isError, write };
+  return { data, isLoading, isSuccess, isError, write, error, status, prepare_error };
 }
