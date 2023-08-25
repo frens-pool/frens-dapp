54d40dd9bcb6281ed4bb0eaad76b1af520660d94
normadn <norman.saade@gmail.com>
2023-08-25 09:26:09 +0200

    fix selectOperator

--- components/operator/SelectOperator.tsx
+++ components/operator/SelectOperator.tsx
@@ -17,86 +17,86 @@ export const SelectOperator = ({
         "https://api.ssv.network/api/v4/prater/operators?type=verified_operator&page=1&perPage=6&ordering=performance.30d%3Adesc"
       );
       const json = await data.json();
-      // console.log(json);
       setssvOperators(json.operators);
     };
 
     fetchOperators().catch(console.error);
   }, []);
 
-  const operatorListRows = ssvOperators?.map((item, i) => (
-    <tr key={i}>
-      <th>
-        <label>
-          <input
-            type="checkbox"
-            className="checkbox"
-            onChange={(e) => {
-              setCheckedOperators(
-                e.target.checked
-                  ? [...checkedOperators, item]
-                  : checkedOperators.filter(
-                    (operator) => operator.id !== item.id
-                  )
-              );
-            }}
-          />
-        </label>
-      </th>
-      <td>
-        <div className="flex items-center space-x-3">
-          <div>
-            <div className="font-bold">{item.name}</div>
-            <div className="text-sm opacity-50">verified</div>
+  let operatorListRows = ssvOperators?.map((item, i) => {
+    return (
+      <tr key={i}>
+        <th>
+          <label>
+            <input
+              type="checkbox"
+              className="checkbox"
+              onChange={(e) => {
+                setCheckedOperators(
+                  e.target.checked
+                    ? [...checkedOperators, item]
+                    : checkedOperators.filter(
+                        (operator) => operator.id !== item.id
+                      )
+                );
+              }}
+            />
+          </label>
+        </th>
+        <td>
+          <div className="flex items-center space-x-3">
+            <div>
+              <div className="font-bold">{item.name}</div>
+              <div className="text-sm opacity-50">verified</div>
+            </div>
           </div>
-        </div>
-      </td>
-      <td>
-        {parseFloat(item.performance["30d"]).toFixed(4)}%
-        <br />
-        <span className="badge badge-ghost badge-sm">last 30d</span>
-      </td>
-      <th>
-        <a
-          href={`https://ssvscan.io/operator/${item.id}`}
-          className="btn btn-ghost btn-xs"
-          target="_blank"
-        >
-          details
-        </a>
-      </th>
-    </tr>
-  );
-}
+        </td>
+        <td>
+          {parseFloat(item.performance["30d"]).toFixed(4)}%
+          <br />
+          <span className="badge badge-ghost badge-sm">last 30d</span>
+        </td>
+        <th>
+          <a
+            href={`https://ssvscan.io/operator/${item.id}`}
+            className="btn btn-ghost btn-xs"
+            target="_blank"
+          >
+            details
+          </a>
+        </th>
+      </tr>
+    );
+  });
 
-return (
-  <div className="my-2 p-2">
-    <div className="overflow-x-auto w-full">
-      <table className="table w-full">
-        <thead>
-          <tr>
-            <th>
-              <label>
-                <input type="checkbox" className="checkbox" disabled />
-              </label>
-            </th>
-            <th>Name</th>
-            <th>Performance</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>{operatorListRows}</tbody>
-      </table>
+  return (
+    <div className="my-2 p-2">
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" disabled />
+                </label>
+              </th>
+              <th>Name</th>
+              <th>Performance</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>{operatorListRows}</tbody>
+        </table>
+      </div>
+      <button
+        className="mt-2 btn bg-gradient-to-r from-frens-blue to-frens-teal text-white"
+        onClick={() => {
+          setOperators(checkedOperators);
+          nextStep();
+        }}
+      >
+        Next
+      </button>
     </div>
-    <button
-      className="mt-2 btn bg-gradient-to-r from-frens-blue to-frens-teal text-white"
-      onClick={() => {
-        setOperators(checkedOperators);
-        nextStep();
-      }}
-    >
-      Next
-    </button>
-  </div>
-);
+  );
 };
