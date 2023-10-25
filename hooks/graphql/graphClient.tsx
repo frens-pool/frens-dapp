056a05e5386e5d0fe742bb076d7babe33e2c49a2
normadn <norman.saade@gmail.com>
2023-10-25 08:59:00 -0700

    use latest frens subgraph

--- hooks/graphql/graphClient.tsx
+++ hooks/graphql/graphClient.tsx
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const APIURL = "https://api.studio.thegraph.com/query/46611/frens-graph/v0.0.1";
+const APIURL =
+  "https://api.studio.thegraph.com/query/46611/frens-graph/version/latest";
 
 export const graphClient = new ApolloClient({
   uri: APIURL,
