d6182264808c86d4bc951e1beb51447a6e59cf8c
normadn <norman.saade@gmail.com>
2023-11-10 11:41:31 -1000

    new pool list

--- components/dashboard/Pools.tsx
+++ components/dashboard/Pools.tsx
@@ -1,8 +1,7 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useQuery, gql } from "@apollo/client";
 import { formatEther } from "viem";
-
-import { UserGroupIcon } from "@heroicons/react/20/solid";
 import { Address } from "wagmi";
 
 interface Deposits {
@@ -39,35 +38,54 @@ export const Pools = () => {
   }
 
   return (
-    <div className="grid grid-cols-2 gap-4 sm:grid-cols-">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
       {data.creates.map((pool: Pool) => (
-        <div
+        <Link
+          href={`/pool/${pool.contractAddress}`}
           key={pool.contractAddress}
           className="relative flex flex-col items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
         >
-          <div>
-            <UserGroupIcon className="-ml-1.5 h-5 w-5" aria-hidden="true" />
-            <Link
-              className="underline text-frens-main"
-              href={`/pool/${pool.contractAddress}`}
-            >
-              {pool.contractAddress}
-            </Link>
-          </div>
-          <div>
-            <div>{pool.deposits.length}</div>
-            <div>
-              {formatEther(
-                BigInt(
-                  pool.deposits?.reduce(
-                    (acc, current) => acc + parseInt(current.amount, 10),
-                    0
-                  )
-                )
-              )}
+          <div className="w-full flex flex-row justify-between">
+            <div className="flex justify-center p-2 mr-6">
+              <Image
+                src="/FRENS-logo-coloured.png"
+                alt="FRENS logo"
+                width="38"
+                height="24"
+              />
+            </div>
+            <div className="w-full grid grid-cols-3 gap-2">
+              <div className="h-full flex flex-col items-center justify-center">
+                <div>Address</div>
+                <div>
+                  {`${pool.contractAddress.slice(
+                    0,
+                    4
+                  )}...${pool.contractAddress.slice(-4)}`}
+                </div>
+              </div>
+
+              <div className="h-full flex flex-col items-center justify-center">
+                <div>Deposits</div>
+                <div className="text-frens-main">{pool.deposits.length}</div>
+              </div>
+              <div className="h-full flex flex-col items-center justify-center">
+                <div>TVL</div>
+                <div className="text-frens-main">
+                  {formatEther(
+                    BigInt(
+                      pool.deposits?.reduce(
+                        (acc, current) => acc + parseInt(current.amount, 10),
+                        0
+                      )
+                    )
+                  )}{" "}
+                  Ξ
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
