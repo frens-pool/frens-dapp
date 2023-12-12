22f2eed08322c3550223f383ea886209a06feca8
normadn <norman.saade@gmail.com>
2023-12-11 17:21:16 -1000

    catch data undefined

--- components/dashboard/Pools.tsx
+++ components/dashboard/Pools.tsx
@@ -39,7 +39,7 @@ export const Pools = () => {
 
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-      {data.creates.map((pool: Pool) => (
+      {data?.creates.map((pool: Pool) => (
         <Link
           href={`/pool/${pool.contractAddress}`}
           key={pool.contractAddress}
