c7db0e2323eac0508b69c3978ef45537df011355
normadn <norman.saade@gmail.com>
2024-09-23 15:00:25 +0200

    use alchemy rpc

--- components/providers.tsx
+++ components/providers.tsx
@@ -27,7 +27,7 @@ const { chains, publicClient } = configureChains(
     jsonRpcProvider({
       rpc: () => {
         return {
-          http: "http://holesky-geth.my.ava.do:8545", //"https://ethereum-holesky.core.chainstack.com/9bd2c053e76cb4859e12390e23609994",
+          http: "https://holesky.infura.io/v3/48089fbe53fa4ad18ffffd3115d11528",
         };
       },
     }),
